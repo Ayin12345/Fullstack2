5bd6e2a1ab2ed0e52f9cd55153ba47cdafe74de0
Ayin12345 <119428134+Ayin12345@users.noreply.github.com>
2024-02-27 17:53:10 -0800

    2.7

---
+++ 2.7.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id: 0,
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(person))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    const names = persons.map(person => person.name)
+    if (names.includes(event.target.value)) {
+        alert(`${event.target.value} is already added to the phonebook`)
+    }
+    else {
+        setNewName(event.target.value)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.id}>{person.name}</li>  
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
