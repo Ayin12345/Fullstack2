b76dac1e963e49438a2c0d5fb09f778bcf2b8033
Ayin12345 <119428134+Ayin12345@users.noreply.github.com>
2024-03-12 16:56:34 -0700

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,116 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import personService from './services/persons';
-import Persons from './components/Persons';
-import PersonForm from './components/PersonForm';
-import Filter from './components/Filter';
-import Notification from './components/Notification';
+import countryService from './services/capitals';
 import './index.css';
 
 const App = () => {
-    const [ persons, setPersons ] = useState([])
-    const [ newName, setNewName ] = useState('')
-    const [ newNumber, setNewNumber ] = useState('')
-    const [ search, setSearch ] = useState('')
-    const [ notificationMessage, setNotificationMessage ] = useState('')
+const [country, setCountries] = useState([])
 
     useEffect(() => {
-        personService
+        countryService
             .getAll()
-            .then(initialPerson => {
-                setPersons(initialPerson);
+            .then(countries => {
+                setCountries(countries)
             })
-    }, [])
-
-    const handleChange = (event) => {
-        if (event.target.name === "name") {
-            setNewName(event.target.value);
-        } else if (event.target.name === "number") {
-            setNewNumber(event.target.value);
-        } else if (event.target.name === "search") {
-            setSearch(event.target.value);
-        }
-    }
-
-    const handleNotificationMessage = (message, type) => {
-      if (type !== 'error') {
-          setNewName('');
-          setNewNumber('');
-      }
-      setNotificationMessage({
-          message: message,
-          type: type
-      })
-      setTimeout(() => {
-          setNotificationMessage(null)
-      }, 5000)
-  }
-
-    const handleDelete = (person) => {
-        const result = window.confirm(`Delete ${person.name} from your phonebook?`);
-        if (result) {
-            personService
-                .remove(person.id)
-                .then(() => {
-                    setPersons(persons.filter(pers => pers.id !== person.id))
-                    handleNotificationMessage(`Deleted ${person.name}`, 'inform')
-                })
-                .catch(error => {
-                    handleNotificationMessage(`Person ${person.name} was already removed from server`, 'error')
-                })
-        }
-    }
-
-    
-
-    const addPerson = (event) => {
-        event.preventDefault();
-
-        if (persons.some((person) => person.name === newName)) {
-            const person = persons.find(pers => pers.name === newName);
-            const result = window.confirm(`${person.name} is already in your phonebook, do you want to replace the old phone number?`);
-            if (result) {
-                const changedPerson = {
-                    ...person,
-                    number: newNumber
-                }
-                personService
-                    .update(person.id, changedPerson)
-                    .then(returnedPerson => {
-                        setPersons(persons.map(pers => pers.id !== returnedPerson.id ? pers : returnedPerson));
-                        handleNotificationMessage(`Updated ${returnedPerson.name}`, 'inform')
-                    })
-                    .catch(error => {
-                      handleNotificationMessage(error.response.data.error, 'error')
-                    })
-            }
-        } else {
-            const personObject = {
-                name: newName,
-                number: newNumber
-            }
-            personService
-                .create(personObject)
-                .then(returnedPerson => {
-                    setPersons(persons.concat(returnedPerson))
-                    handleNotificationMessage(`Added ${returnedPerson.name}`, 'inform')
-                })
-                .catch(error => {
-                  handleNotificationMessage(error.response.data.error, 'error')
-                })
-        }
-    }
-
+    })
 
     return (
         <div>
-            <h2>Phonebook</h2>
-            <Notification message={notificationMessage} />
-            <h3>Search</h3>
-            <Filter search={search} onChange={handleChange} />
-            <h3>Add a Number</h3>
-            <PersonForm onSubmit={addPerson} newName={newName} newNumber={newNumber} onChange={handleChange} />
-            <h3>Numbers</h3>
-            <Persons personsToShow={persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))} handleDelete={handleDelete} />
+            find countries <input></input>
+            <ul>
+                {country.map(place => 
+                    <li>{place.name.common}</li>
+                )}
+            </ul>
         </div>
     )
 }
