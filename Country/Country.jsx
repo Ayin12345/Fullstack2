4a378b81414b290192f057f5430a4e178029d8ba
Ayin12345 <119428134+Ayin12345@users.noreply.github.com>
2024-03-20 16:32:13 -0700

    Update Country.jsx

--- Country/Country.jsx
+++ Country/Country.jsx
@@ -1,38 +1,14 @@
 import React from 'react';
+import Details from './Details';
+import Weather from './Weather';
 
-const Country = (props) => {
-    if (props.newCountry[0] == "Too many matches") {
-        return (
-            <div>
-                <p>{props.newCountry[0]}</p>
-            </div>
-        )
-    } else if (props.newCountry.length === 1 ) {
-        const place = props.newCountry[0]
-        return (
-            <div>
-                <h2>{place.name.common}</h2>
-                <p>Capital {place.capital}</p>
-                <p>Area {place.area}</p>
-                Languages:
-                <ul>
-                    {Object.keys(place.languages).map((key) =>
-                        <li>{place.languages[key]}</li>
-                    )}
-                </ul>
-                <br></br>
-                <img src={place.flags.png} alt={place.flags.alt}></img>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <ul>
-                    {props.newCountry.map((item) => <li>{item.name.common}</li>)}
-                </ul>
-            </div>
-        )
-    }
+const Country = ({ country, weather }) => {
+    return (
+        <div>
+            <Details country={country} />
+            <Weather country={country} weather={weather} />
+        </div>
+    )
 }
 
 export default Country;
