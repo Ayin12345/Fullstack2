fb5aebc884a355cdbc3e5a9701ecc567fe3c07b7
Ayin12345 <119428134+Ayin12345@users.noreply.github.com>
2024-03-20 16:31:34 -0700

    Update App.jsx

--- Country/App.jsx
+++ Country/App.jsx
@@ -1,45 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import countryService from './services/capitals';
-import './index.css';
-import Country from './components/Country'
+import axios from 'axios';
+import Search from './Search';
+import Result from './Result';
+import Country from './Country';
 
 const App = () => {
-const [country, setCountries] = useState([])
-const [newCountry, setNewCountries] = useState([])
+    const [ search, setSearch ] = useState('');
+    const [ countries, setCountries ] = useState([]);
+    const [ weather, setWeather ] = useState({});
+    const WEATHER_API_KEY = import.meta.env.VITE_SOME_KEY || '';
+    let checked = 0
 
     useEffect(() => {
-        countryService
-            .getAll()
-            .then(countries => {
-                setCountries(countries)
+        axios
+            .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+            .then(response => {
+                setCountries(response.data);
             })
-    })
+    }, [])
 
-    const handleInput = (event) => {
-        const newValues = []
-        let counter = 0
-        country.map(place => {
-            if (place.name.common.toLowerCase().includes(event.target.value.toLowerCase())) {
-                counter++
-                newValues.push(place)
-            }
-        })
-        if (counter > 10) {
-            setNewCountries(["Too many matches"])
-        } else {
-            setNewCountries(newValues)
+    useEffect(() => {
+        checked = 1
+        const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()));
+        if (countriesToShow.length === 1) {
+            const country = countriesToShow[0]
+            axios
+                .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country["capitalInfo"]["latlng"][0]}&lon=${country["capitalInfo"]["latlng"][1]}&units=metric&APPID=${WEATHER_API_KEY}`)
+                .then(response => {
+                    setWeather(response);
+                })
         }
+    }, [countries, search])
+
+    const handleChange = (event) => {
+        setSearch(event.target.value);
     }
 
+    const handleClick = (event) => {
+        event.preventDefault();
+        setSearch(event.target.name);
+    }
+    const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()));
+    console.log("1")
     return (
         <div>
-            find countries <input
-                onChange={handleInput} />
-            <ul>
-                <Country country={country} newCountry={newCountry}/>
-            </ul>
+            <Search search={search} handleChange={handleChange} />
+            {countriesToShow.length !== 1 ? <Result countriesToShow={countriesToShow} handleClick={handleClick} /> : <Country country={countriesToShow[0]} weather={weather} />}
         </div>
     )
+
 }
 
-export default App
+export default App;
